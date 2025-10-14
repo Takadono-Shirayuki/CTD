a6f83b7538042813200bb1069f2a8a5143853f56
Takadono-Shirayuki <Thanh.BT225669@sis.hust.edu.vn>
2025-10-14 21:38:19 +0700

    Update client.c

--- BangTienThanh_20225669_HW3/UDP_Client/client.c
+++ BangTienThanh_20225669_HW3/UDP_Client/client.c
@@ -15,29 +15,58 @@ int main(int argc, char *argv[]) {
 
     int port = atoi(argv[2]);
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        fprintf(stderr, "Socket creation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    
     struct sockaddr_in server_addr;
     socklen_t server_len = sizeof(server_addr);
-    char *buffer = NULL;
+    char response_buffer[UDP_BUFFER_SIZE];
 
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port);
-    inet_pton(AF_INET, argv[1], &server_addr.sin_addr);
+    if (inet_pton(AF_INET, argv[1], &server_addr.sin_addr) <= 0) {
+        fprintf(stderr, "Invalid server IP address\n");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
 
     while (1) {
         printf("Enter domain name or IPv4 address (or press Enter to quit): ");
-        buffer = get_long_stdin_input('\n');
-        buffer[strcspn(buffer, "\n")] = 0;
+        
+        char *input_buffer = get_long_stdin_input('\n');
+        if (input_buffer == NULL) {
+            // EOF or error, exit gracefully
+            break;
+        }
+        
+        // Remove newline if present
+        input_buffer[strcspn(input_buffer, "\n")] = 0;
 
-        if (strlen(buffer) == 0) break;
+        if (strlen(input_buffer) == 0) {
+            free(input_buffer);
+            break;
+        }
 
-        sendto(sockfd, buffer, strlen(buffer), 0,
+        // Send request
+        sendto(sockfd, input_buffer, strlen(input_buffer), 0,
                (struct sockaddr*)&server_addr, server_len);
 
-        memset(buffer, 0, UDP_BUFFER_SIZE);
-        recvfrom(sockfd, buffer, UDP_BUFFER_SIZE, 0,
-                 (struct sockaddr*)&server_addr, &server_len);
-
-        printf("Result: %s\n", buffer);
+        // Receive response in separate buffer
+        memset(response_buffer, 0, UDP_BUFFER_SIZE);
+        int bytes_received = recvfrom(sockfd, response_buffer, UDP_BUFFER_SIZE - 1, 0,
+                                    (struct sockaddr*)&server_addr, &server_len);
+        
+        if (bytes_received > 0) {
+            response_buffer[bytes_received] = '\0';
+            printf("Result: %s\n", response_buffer);
+        } else {
+            printf("Error receiving response\n");
+        }
+        
+        // Free input buffer after use
+        free(input_buffer);
     }
 
     close(sockfd);
